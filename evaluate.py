6fb41c532ec90fdba8d2a7ac6af7f509b2c6d0ca
pierre-parel <pierre_parel@dlsu.edu.ph>
2024-12-20 11:15:39 +0800

    Fix testing dataset loading

--- evaluate.py
+++ evaluate.py
@@ -6,43 +6,16 @@ from tensorflow import keras
 
 from keras.saving import load_model
 
-loaded_model = load_model("./saved_models/60-0.93.keras") # Replace with model to be tested.
+loaded_model = load_model("./saved_models/95-0.94.keras") # Replace with model to be tested.
 
 IMG_SIZE = (224, 224)
-BATCH_SIZE = 64
-EPOCHS = 100
-ds_train, ds_test = keras.utils.image_dataset_from_directory(
+ds_test = keras.utils.image_dataset_from_directory(
     "coffee_bean_test",
-    validation_split=0.2,
-    subset="both",
-    seed=123,
+    seed=1337,
     image_size=IMG_SIZE,
-    batch_size=BATCH_SIZE,
     label_mode="categorical",
 )
 
 loaded_model.evaluate(
-    ds_train
-)
-loaded_model.evaluate(
     ds_test
 )
-
-# Output ranges from 0-16, corresponding to each class (in alphabetical order)
-# 0 = Broken
-# 1 = Cut
-# 2 = Dry Cherry
-# 3 = Fade
-# 4 = Floater
-# 5 = Full Black
-# 6 = Full Sour
-# 7 = Fungus Damage
-# 8 = Husk
-# 9 = Immature
-# 10 = Parchment
-# 11 = Partial Black
-# 12 = Partial Sour
-# 13 = Severe Insect Damage
-# 14 = Shell
-# 15 = Slight Insect Damage
-# 16 = Withered
